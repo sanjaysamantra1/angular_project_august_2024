36c2deb5a7b80b8fc39f34b8193e722c0ce1e820
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-09-05 08:30:40 +0530

    05 sept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BestDealsComponent } from './components/best-deals/best-deals.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    BestDealsComponent,
+    FooterComponent,
+    BodyComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/best-deals/best-deals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-best-deals',
+  standalone: true,
+  imports: [],
+  templateUrl: './best-deals.component.html',
+  styleUrl: './best-deals.component.css'
+})
+export class BestDealsComponent {
+
+}
