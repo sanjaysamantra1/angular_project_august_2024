7034de883c30921ba51936693e0e7b006594ff4e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-09-20 08:29:46 +0530

    impure pipe

--- src/app/components/pipes-demo/pipes-demo.component.ts
+++ src/app/components/pipes-demo/pipes-demo.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RemainingPipe } from '../../pipes/remaining.pipe';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { MySortPipe } from '../../pies/my-sort.pipe';
 
 @Component({
   selector: 'app-pipes-demo',
@@ -11,24 +12,27 @@ import { TruncatePipe } from '../../pipes/truncate.pipe';
     CommonModule,
     FormsModule,
     RemainingPipe,
-    TruncatePipe
+    TruncatePipe,
+    MySortPipe
   ],
   templateUrl: './pipes-demo.component.html',
   styleUrl: './pipes-demo.component.css'
 })
 export class PipesDemoComponent {
   myName: string = 'saCHin tEnDuLKaR';
+  myAge: number = 20;
   mySal: number = 5000;
   today: Date = new Date();
   user = { name: 'sachin', add: 'mumbai', sal: 5000 };
   cars = ['Tata', 'Honda', 'Maruti', 'Hundai', 'Toyota', 'Mahindra'];
-  // userPromise: any;
+  userPromise: Promise<any>;
   msg: string = 'Hello';
   msg2: string = 'Hello';
-  longStr:string = 'Hello Hiiiiiiiiiii, I am a Long String. Heloooooooooo HIiiiiiiii'
+  longStr: string = 'Hello Hiiiiiiiiiii, I am a Long String. Heloooooooooo HIiiiiiiii'
 
   constructor() {
-    // this.userPromise = fetch('https://jsonplaceholder.typicode.com/users');
-    // console.log(this.userPromise)
+    this.userPromise = fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
   }
+
+  numArr = [40, 50, 10, 30, 20];
 }
--- src/app/pipes/remaining.pipe.ts
+++ src/app/pipes/remaining.pipe.ts
@@ -2,10 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'remaining',
-  standalone: true
+  standalone: true,
+  // pure: false // Impure Pipe
 })
 export class RemainingPipe implements PipeTransform {
-  transform(msg: any, maxlength:any) {
+  transform(msg: any, maxlength: any) {
+    console.log('Remaining Pipe Called')
     return maxlength - msg.length;
   }
 }
